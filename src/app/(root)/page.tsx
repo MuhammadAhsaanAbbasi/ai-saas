1fb7899ecb4863aaf163d1933d7806731a0f74ea
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-03-16 00:04:36 +0500

    Auth:

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,10 +3,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
+    <>
+    <div>
+      <p>Hello World</p>
       <UserButton afterSignOutUrl="/"/>
       <SignInButton/>
-    </main>
+    </div>
+    </>
   );
 }
