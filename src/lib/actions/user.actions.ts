3f0dea9ed4ff1cb9b1a1a223e4283736346edade
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-03-23 14:46:03 +0500

    db use server

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,64 +1,74 @@
+"use server";
+
 import { revalidatePath } from "next/cache";
 
 import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 
-// Create
+// CREATE
 export async function createUser(user: CreateUserParams) {
-    try{
+    try {
         await connectToDatabase();
 
         const newUser = await User.create(user);
+
         return JSON.parse(JSON.stringify(newUser));
-    }catch(error){
+    } catch (error) {
         handleError(error);
     }
 }
 
-// Read
+// READ
 export async function getUserById(userId: string) {
-    try{
+    try {
         await connectToDatabase();
-        const user = await User.findOne({clerkId: userId});
 
-        if(!user) throw new Error("User not found");
+        const user = await User.findOne({ clerkId: userId });
+
+        if (!user) throw new Error("User not found");
+
         return JSON.parse(JSON.stringify(user));
-    }catch(error){
+    } catch (error) {
         handleError(error);
     }
 }
 
-// Update
+// UPDATE
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
-    try{
+    try {
         await connectToDatabase();
 
-        // Update user
-        const updatedUser = await User.findOneAndUpdate({clerkId}, user, {new: true});
-        if(!updatedUser) throw new Error("User not found");
+        const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+            new: true,
+        });
+
+        if (!updatedUser) throw new Error("User update failed");
 
         return JSON.parse(JSON.stringify(updatedUser));
-    }catch(error){
+    } catch (error) {
         handleError(error);
     }
 }
 
-// Delete
+// DELETE
 export async function deleteUser(clerkId: string) {
-    try{
+    try {
         await connectToDatabase();
 
-        // Search user by Id
-        const userToDelete = await User.findOne({clerkId});
-        if(!userToDelete) throw new Error("User not found");
+        // Find user to delete
+        const userToDelete = await User.findOne({ clerkId });
 
-        // User Deleted By Id
+        if (!userToDelete) {
+            throw new Error("User not found");
+        }
+
+        // Delete user
         const deletedUser = await User.findByIdAndDelete(userToDelete._id);
         revalidatePath("/");
 
-        return JSON.parse(JSON.stringify(deletedUser));
-    }catch(error){
+        return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+    } catch (error) {
         handleError(error);
     }
 }
--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -1,4 +1,4 @@
-import mongoose, {Mongoose} from "mongoose";
+import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
@@ -8,6 +8,7 @@ interface MongooseConnection {
 }
 
 let cached: MongooseConnection = (global as any).mongoose
+
 if (!cached) {
     cached = (global as any).mongoose = {
         conn: null, promise: null
@@ -15,13 +16,17 @@ if (!cached) {
 }
 
 export const connectToDatabase = async () => {
-    if (cached.conn) {
-        return cached.conn;
-    }
-    
-    if(!MONGODB_URL) throw new Error("MONGODB_URL is not defined");
-    cached.promise = cached.promise || 
-    mongoose.connect(MONGODB_URL, {dbName: "lensaai", bufferCommands: false});
+    if (cached.conn) return cached.conn;
+
+    if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+    cached.promise =
+        cached.promise ||
+        mongoose.connect(MONGODB_URL, {
+            dbName: 'lensai', bufferCommands: false
+        })
+
     cached.conn = await cached.promise;
+
     return cached.conn;
 }
