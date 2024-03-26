7677121080f7b0e66e61b2d590b91ab00bdbcd26
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-03-26 06:55:31 +0500

    build pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Sans } from "next/font/google";
-import { cn } from "@/lib/utils";
-import { ClerkProvider } from "@clerk/nextjs";
-
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+import { ClerkProvider } from '@clerk/nextjs'
 
-const IBMPlex = IBM_Plex_Sans({ 
-  subsets: ["latin"],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-ibm-plex'
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Lensa A.i",
@@ -26,14 +19,12 @@ export default function RootLayout({
     <ClerkProvider appearance={{
       variables: { colorPrimary: '#624cf5' }
     }}>
-      <html lang="en">
-        <head>
-        <link rel="icon" href="./assets/favicon.ico" />
-        </head>
-        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-          {children}
-        </body>
-      </html>
+    <html lang="en">
+      <head>
+      <link rel="icon" href="./assets/favicon.ico" />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
     </ClerkProvider>
   );
 }
